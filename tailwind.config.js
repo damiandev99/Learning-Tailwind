4830880f00e3fd587ae93c77f50ff5f591a1058c
damiandev99 <doriandamianv29@gmail.com>
2024-09-19 22:32:04 -0500

    Creacion de la seccion de las categorias del blog y la colocacion del boton centrado en cada etiqueta para que redirija a la opcion requerida

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     extend: {
       backgroundImage:{
         'banner': "url('./public/img/banner.svg')",
+        'ciberseguridad': "url(./public/img/ciberseguridad.jpg)",
+        'software': "url(./public/img/software.jpg)",
+        'ia': "url(./public/img/ia.jpg)",
       }
     },
   },
